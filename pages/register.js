1fb531e24d379d83bffb7adf49855d855f6b9a5d
Chinmay Milind Tullu <chinmaytullu15@gmail.com>
2024-02-01 00:23:16 +0530

    code addtion register

--- pages/register.js
+++ pages/register.js
@@ -1,10 +1,15 @@
+let own=document.querySelector("#owner");
 let cont=document.querySelector("#contractor");
 let fam=document.querySelector("#member");
 
+own.addEventListener("click", () => {
+    document.querySelector("#number").style.visibility="hidden";
+});
+
 cont.addEventListener("click", () => {
-    console.log("hi");
+    document.querySelector("#number").style.visibility="visible";
 });
 
 fam.addEventListener("click", () => {
-    console.log("hi");
+    document.querySelector("#number").style.visibility="visible";
 })
