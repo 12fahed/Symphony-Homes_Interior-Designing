32f05f848e3a52850d4f890eb6a1947f653da9ba
Chinmay Milind Tullu <chinmaytullu15@gmail.com>
2024-01-31 10:50:38 +0530

    updated Admin.js

--- api/models/Admin.js
+++ api/models/Admin.js
@@ -4,7 +4,8 @@ const AdminSchema=mongoose.Schema({
     name:String,
     email:{type:String,unique:true},
     password:String,
-    code:{type:String,unique:true}
+    code:{type:String,unique:true},
+    mobile: {type: String, unique: true}
 
 })
 const adminModel=mongoose.model('User',AdminSchema)
